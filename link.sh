18d4ad56d79e7937626676daddf175ad0d20a03b
Andrea Lattuada <utaaal@gmail.com>
2012-11-29 22:37:18 +0100

    updated scripts

--- link.sh
+++ link.sh
@@ -4,6 +4,6 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/vim ~/.vim
-
 #ln -s gitconfig ~/.gitconfig
 
+git submodule update --init
