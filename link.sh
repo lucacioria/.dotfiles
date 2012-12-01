fc822a6fdbd78aa3b026ece24dfa5bc937d08234
Andrea Lattuada <utaaal@gmail.com>
2012-12-01 22:46:19 +0100

    zsh

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh ~/.zsh
 #ln -s gitconfig ~/.gitconfig
 
 git submodule update --init
