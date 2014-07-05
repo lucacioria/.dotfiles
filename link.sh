3856a082ad520d56835e5a98108be6a193856d1c
Luca Cioria <vircheck@gmail.com>
2014-07-05 15:20:27 +0200

    mackup.cfg is already backed up by mackup itself

--- link.sh
+++ link.sh
@@ -5,6 +5,5 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh ~/.zsh
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/mackup.cfg ~/.mackup.cfg
 
 git submodule update --init
