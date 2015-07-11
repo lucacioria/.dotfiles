3651c887e06ea27f3276d6450aeb424db488c63d
Luca Cioria <vircheck@gmail.com>
2015-07-11 15:20:15 +0200

    remove/add aliases... private luca-only stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
-alias push_oxway='cd ~/Documents/buildo/repo/oxway/web; rm -r target; broccoli build target;rsync -vrzhe ssh --progress target/ oxway@95.85.18.31:/usr/share/nginx/html/;'
-alias run_dendrite='cd $HOME/Documents/buildo/repo/labonline/dendrite; nvm use 0.11; node --harmony app.js;'
 alias run_docs_escalapio='cd $HOME/Documents/buildo/repo/labonline/documentation/doc-parser; git pull --ff-only; node index.js -i ../documentation.sbraml -o ../documentation.html; open ../documentation.html'
 alias l=l -lah
+alias backup_cashflow='mongodump --host 5.196.128.125 -o $HOME/Documents/buildo/cashflow/dump$(date "+%Y-%m-%d-%H-%M")'
+
