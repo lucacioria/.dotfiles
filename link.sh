73068cced11efe5da45d88bfb9fd0e40bc3c5749
Luca Cioria <vircheck@gmail.com>
2015-07-11 15:18:37 +0200

    added vifm file manager

--- link.sh
+++ link.sh
@@ -6,5 +6,6 @@ ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh ~/.zsh
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitignore ~/.gitignore
+ln -s ~/.dotfiles/vifmrc ~/.vifmrc
 
 git submodule update --init
