82d5e5bf4281951ff4c73eac0ad61dacafcb1246
Luca Cioria <vircheck@gmail.com>
2015-07-13 16:00:39 +0200

    alias git to hub

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias run_docs_escalapio='cd $HOME/Documents/buildo/repo/labonline/documentation/doc-parser; git pull --ff-only; node index.js -i ../documentation.sbraml -o ../documentation.html; open ../documentation.html'
 alias l=l -lah
 alias backup_cashflow='mongodump --host 5.196.128.125 -o $HOME/Documents/buildo/cashflow/dump$(date "+%Y-%m-%d-%H-%M")'
+alias git=hub
 
